0ff25bf693ab60e9e844a312e30d40ccdee7d5fe
sivaranjani266 <sivaranjani26.arul@gmail.com>
2022-01-19 08:32:32 +0000

    tsrt-frst-commit

---
+++ testarray.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import printer;
+
+public class Array {
+
+    public static void main(String[] args) {
+        int[][] array = {{1, 2}, {3, 4}, {5, 6, 7}};
+
+        System.out.println(Arrays.deepToString(array));
+    }
+}
